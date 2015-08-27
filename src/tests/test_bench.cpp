ed00de84390a70d0655c008b342b0735a285d600
Kapp Arnaud <kapp.arno@gmail.com>
2015-08-28 00:36:16 +0200

    add perf test

---
+++ src/tests/test_bench.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "ell.hpp"
+
+int incr()
+{
+    return 42;
+}
+
+int count_to_some_number()
+{
+    std::cout << "~~Tata~~" << std::endl;
+    int count = 0;
+    for (int i = 0; i < 10000000; i++)
+    {
+        count += ell::yield(incr);
+    }
+    std::cout << "Continuing tata..." << count << std::endl;
+    return 1337;
+}
+
+int main()
+{
+    ell::EventLoop l;
+    auto task = l.call_soon(count_to_some_number);
+    l.run_until_complete(task);
+
+    std::cout << "Result = " << task->get_result() << std::endl;
+    return 0;
+}
