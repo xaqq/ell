625e5292ab906f07b009fdb1099a6ff213928447
Kapp Arnaud <kapp.arno@gmail.com>
2015-08-30 02:53:33 +0200

    format

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -3,31 +3,31 @@
 
 int tata()
 {
-    std::cout << "~~Tata~~" << std::endl;
-    for (int i = 0; i < 2000000; i++)
-    {
-        ell::yield();
-    }
-    std::cout << "Continuing tata..." << std::endl;
-    return 1337;
+  std::cout << "~~Tata~~" << std::endl;
+  for (int i = 0; i < 2000000; i++)
+  {
+    ell::yield();
+  }
+  std::cout << "Continuing tata..." << std::endl;
+  return 1337;
 }
 
 int toto()
 {
-    std::cout << "~~Toto~~" << std::endl;
-    ell::yield();
-    std::cout << "~~Titi~~" << std::endl;
+  std::cout << "~~Toto~~" << std::endl;
+  ell::yield();
+  std::cout << "~~Titi~~" << std::endl;
 
-    std::cout << "Return value from Tata: " << ell::yield(tata) << std::endl;
-    return 42;
+  std::cout << "Return value from Tata: " << ell::yield(tata) << std::endl;
+  return 42;
 }
 
 int main()
 {
-    ell::EventLoop l;
-    auto task = l.call_soon(toto);
-    l.run_until_complete(task);
+  ell::EventLoop l;
+  auto task = l.call_soon(toto);
+  l.run_until_complete(task);
 
-    std::cout << "Result = " << task->get_result() << std::endl;
-    return 0;
+  std::cout << "Result = " << task->get_result() << std::endl;
+  return 0;
 }
--- src/tests/test_bench.cpp
+++ src/tests/test_bench.cpp
@@ -3,27 +3,27 @@
 
 int incr()
 {
-    return 42;
+  return 42;
 }
 
 int count_to_some_number()
 {
-    std::cout << "~~Tata~~" << std::endl;
-    int count = 0;
-    for (int i = 0; i < 100000; i++)
-    {
-        count += ell::yield(incr);
-    }
-    std::cout << "Continuing tata..." << count << std::endl;
-    return 1337;
+  std::cout << "~~Tata~~" << std::endl;
+  int count = 0;
+  for (int i = 0; i < 10000000; i++)
+  {
+    count += ell::yield(incr);
+  }
+  std::cout << "Continuing tata..." << count << std::endl;
+  return 1337;
 }
 
 int main()
 {
-    ell::EventLoop l;
-    auto task = l.call_soon(count_to_some_number);
-    l.run_until_complete(task);
+  ell::EventLoop l;
+  auto task = l.call_soon(count_to_some_number);
+  l.run_until_complete(task);
 
-    std::cout << "Result = " << task->get_result() << std::endl;
-    return 0;
+  std::cout << "Result = " << task->get_result() << std::endl;
+  return 0;
 }
